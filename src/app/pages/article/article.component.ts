017f3de55eb1d5e1740098831fc7f424472509aa
Dany Paredes <danywalls4@gmail.com>
2021-12-27 17:11:19 +0100

    Merge commit 'ba45c7b8c23671a67fb6d440e40a9b51d7948f8e'

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -3,17 +3,9 @@ import { Article } from '../search/services/search.service';
 
 @Component({
   selector: 'app-article',
-  template: `
-  <article class="article">
-    <a [href]="'https://es.wikepedia.org/?curid='+ article.pageid " target="_blank">
-      {{article.title}}
-    </a>
-    <p [innerHTML]="article.snippet"></p>
-  </article>
-  `,
-  styleUrls: ['./article.component.scss']
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent {
   @Input() article!: Article;
-
 }
